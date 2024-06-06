8493b3b81abea1dd19cff97d4d8e74ab372c813c
= <=>
2024-06-06 12:55:19 +0530

    added comment

--- Utility/utility_file.py
+++ Utility/utility_file.py
@@ -3,4 +3,4 @@ from configparser import ConfigParser
 def get_config(category,key):
     con = ConfigParser()
     con.read("E:\\sample pytest\\Pytest_OrangeHRM\\Pytest_OrangeHRM\Utility\\config.ini")
-    return con.get(category,key)
+    return con.get(category,key) #return here
